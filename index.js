8423228c20c1a9edf3024fa2e7eab1f73f2b196b
SEANGLAY <liseanglay@gmail.com>
2023-07-11 11:04:50 +0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.get("/", (req, res) => res.send("It worked"));
 
+let item = [
+  { id: 1, title: "ReactJS" },
+  { id: 2, title: "NextJS" },
+  { id: 3, title: "React Native" },
+];
+
+app.get("/test", (req, res) => res.json(item));
+
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
